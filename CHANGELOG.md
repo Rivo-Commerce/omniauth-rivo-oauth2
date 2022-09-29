d4ddef2c9922ed1768630827fe12008df26df578
Summer Megel <summer@thoughtbot.com>
2022-09-29 15:33:40 -0600

    Reset version to 0.0.0/unreleased

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,3 @@
 ## [Unreleased]
 
-## [0.1.0] - 2022-09-29
-
 - Initial release
--- lib/omniauth/rivo/oauth2/version.rb
+++ lib/omniauth/rivo/oauth2/version.rb
@@ -3,7 +3,7 @@
 module Omniauth
   module Rivo
     module Oauth2
-      VERSION = "0.1.0"
+      VERSION = "0.0.0"
     end
   end
 end
